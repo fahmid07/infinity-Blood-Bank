c0eb4391d5efa79db93c858dac2f783c418577d3
Fahmidur Rahman Orpon <fahmidurrahman07@gmail.com>
2021-08-07 17:54:03 +0600

    src

--- infinity Blood bank/app/src/main/java/com/example/infinitybloodbank/MainActivity.java
+++ infinity Blood bank/app/src/main/java/com/example/infinitybloodbank/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.infinitybloodbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -18,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     Animation top, bottom;
     ImageView logo;
     TextView slogan;
-    private static int SPLASH_SCREEN = 4000;
+    private static int SPLASH_SCREEN = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +42,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 Intent intent = new Intent(MainActivity.this, Login.class);
-                startActivity(intent);
-                finish();
+
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View, String>(logo, "logo_image");
+                pairs[1] = new Pair<View, String>(slogan, "logo_text");
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, pairs);
+                startActivity(intent, options.toBundle());
             }
         }, SPLASH_SCREEN);
     }
