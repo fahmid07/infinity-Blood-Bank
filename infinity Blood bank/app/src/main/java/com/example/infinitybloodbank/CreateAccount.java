e5f276ab9cd6ee1b82e93518b58ba91b29701925
Fahmidur Rahman Orpon <fahmidurrahman07@gmail.com>
2021-08-07 22:05:29 +0600

    update

--- infinity Blood bank/app/src/main/java/com/example/infinitybloodbank/CreateAccount.java
+++ infinity Blood bank/app/src/main/java/com/example/infinitybloodbank/CreateAccount.java
@@ -3,12 +3,29 @@ package com.example.infinitybloodbank;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class CreateAccount extends AppCompatActivity {
+
+    AutoCompleteTextView dst, bld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        dst = findViewById(R.id.districttxt);
+        bld = findViewById(R.id.bloodtxt);
+
+        String [] districts = {"Bandarban","Brahmanbaria",   "Chandpur", "Chittagong", "Comilla",    "Cox's Bazar","Feni",     "Khagrachhari","Lakshmipur", "Noakhali", "Rangamati", "Barguna",  "Barisal",        "Bhola",    "Jhalokati",  "Patuakhali", "Pirojpur", "Dhaka",    "Faridpur",       "Gazipur",  "Gopalganj",  "Kishoreganj","Madaripur",  "Manikganj","Munshiganj",  "Narayanganj","Narsingdi","Rajbari","Shariatpur","Tangail", "Bagerhat", "Chuadanga",      "Jessore",  "Jhenaidah",  "Khulna",     "Kushtia",    "Magura",   "Meherpur",    "Narail",     "Satkhira", "Jamalpur", "Mymensingh",     "Netrakona","Sherpur", "Bogra",    "Chapainawabganj","Joypurhat","Naogaon",    "Natore",     "Pabna",      "Rajshahi", "Sirajganj", "Dinajpur", "Gaibandha",      "Kurigram", "Lalmonirhat","Nilphamari", "Panchagarh", "Rangpur",  "Thakurgaon", "Habiganj", "Moulvibazar",    "Sunamganj","Sylhet"};
+        ArrayAdapter dstr = new ArrayAdapter(this, R.layout.district_item, districts);
+        //dst.setText(dstr.getItem(0).toString(), false);
+        dst.setAdapter(dstr);
+
+        String [] bloods = {"B+", "B-", "A+", "A-", "O+", "O-", "AB+", "AB-"};
+        ArrayAdapter bldg = new ArrayAdapter(this, R.layout.district_item, bloods);
+        //bld.setText(bldg.getItem(0).toString(), false);
+        bld.setAdapter(bldg);
     }
 }
--- infinity Blood bank/app/src/main/java/com/example/infinitybloodbank/Login.java
+++ infinity Blood bank/app/src/main/java/com/example/infinitybloodbank/Login.java
@@ -2,15 +2,45 @@ package com.example.infinitybloodbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
+    Button in, up, skp, frgt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        in = findViewById(R.id.signin);
+        up = findViewById(R.id.createac);
+        skp = findViewById(R.id.skip);
+        frgt = findViewById(R.id.forgot);
+    }
+
+    public void CreateButtonClick(View v){
+        Intent i = new Intent(this, CreateAccount.class);
+        startActivity(i);
+    }
+
+    public void SkipButtonClick(View v){
+        Intent i = new Intent(this, CreateAccount.class);
+        startActivity(i);
+    }
+
+    public void SignButtonClick(View v){
+        Intent i = new Intent(this, CreateAccount.class);
+        startActivity(i);
+    }
+
+    public void ForgotButtonClick(View v){
+        Intent i = new Intent(this, CreateAccount.class);
+        startActivity(i);
     }
 }
